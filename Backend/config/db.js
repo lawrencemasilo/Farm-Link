c446de311df7c405dd88baa52f4289febf013c52
Sicelumusa <musaqwabe@gmail.com>
2024-05-19 12:48:54 +0200

    Implemented global error handling, basic express server and database configuration

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,1 +1,14 @@
 // Handle database connection setup
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+}).promise();
+
+module.exports = pool;
---
+++ Backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express()
+
+const dotenv =require('dotenv');
+const errorMiddleware = require('./middleware/errors');
+const ErrorHandler = require('./utils/errorHandler');
+
+// Setting up .env file variables
+dotenv.config();
+
+// Handling Uncaught Exception
+process.on('uncaughtException', err => {
+  console.log(`ERROR: ${err.message}`);
+  console.log('Shutting down due to uncaught exception');
+  process.exit(1);
+}) 
+
+// The routes start here
+
+
+
+
+// The routes end here
+
+// Handling unhandled routes
+app.all('*', (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
+
+// Middleware to handle errors
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
+
+// Handling Unhandled Prommise Rejection
+process.on('unhandledRejection', err => {
+  console.log(`Error: ${err.message}`);
+  console.log('Shutting down the server due to unhandled promise rejection');
+  server.close( () => {
+    process.exit(1);
+  })
+}); 
