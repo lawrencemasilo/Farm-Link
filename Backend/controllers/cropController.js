cabc9d4e05a8827138003bd857e29e683b6d3a0e
Simamnkele <smambo85@gmail.com>
2024-05-21 00:27:28 +0200

    Created cropController

---
+++ Backend/controllers/cropController.js
@@ -0,0 +1,9 @@
+// Logic for crop-related operations
+const Crop = require('../models/cropModel');
+const User = require('../models/userModel');
+const Farm = require('../models/farmModel');
+
+// Allow user to update crop details
+const updateCropDetails = () => {
+    // Will add logic later today :/
+};
