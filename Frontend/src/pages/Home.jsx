656e5da83900144f0645e30082294ab7e68eb3fd
Neo Masilo <neolawrencemasilo@gmail.com>
2024-05-22 00:02:31 +0200

    imported the Header and SideBar components

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import '../styles/Home.css'
 import HeaderSignIn from '../components/HeaderSignIn'
+import SideBar from '../components/SideBar'
+import Schedule from '../components/Schedule'
+import Members from '../components/Members'
+import { useState } from 'react'
 
 export default function Home() {
+  const [selectedSchedule, setSelectedSchedule] = useState('schedule');
+
   return (
     <div className="home-container">
-      {<HeaderSignIn />}
+      <div className="home-header-container">
+        {<HeaderSignIn />}
+      </div>
+      <div className="main-content-container">
+        {<SideBar setSelectedSchedule={setSelectedSchedule}/>}
+        {selectedSchedule === 'schedule' ? <Schedule /> : <Members />}
+      </div>
     </div>
   )
 }
