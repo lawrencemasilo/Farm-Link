009e0efe0a1f49ffe97602c615dd8da6b76dfcd7
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-24 19:20:30 +0200

    addes more objects

--- Frontend/src/dataTest.js
+++ Frontend/src/dataTest.js
@@ -35,6 +35,78 @@ const farmersData = [
       cropType: 'Corn',
       production: '900 kg'
     },
+    {
+      id: 5,
+      name: 'Tom',
+      email: 'tom@gmail.com',
+      address: 'Thembisa',
+      farmSize: '10 hectare',
+      cropType: 'Corn',
+      production: '60 kg'
+    },
+    {
+      id: 6,
+      name: 'Mary',
+      email: 'Mary@gmail.com',
+      address: 'Thembisa',
+      farmSize: '56 hectare',
+      cropType: 'Beans',
+      production: '500 kg'
+    },
+    {
+      id: 7,
+      name: 'Sandile',
+      email: 'sandile@gmail.com',
+      address: 'Thembisa',
+      farmSize: '49 hectare',
+      cropType: 'Tomato',
+      production: '1200 kg'
+    },
+    {
+      id: 8,
+      name: 'Lwazi',
+      email: 'ulwazi@gmail.com',
+      address: 'Thembisa',
+      farmSize: '120 hectare',
+      cropType: 'Corn',
+      production: '5000 kg'
+    },
+    {
+      id: 9,
+      name: 'Bongani',
+      email: 'bongani@gmail.com',
+      address: 'Thembisa',
+      farmSize: '59 hectare',
+      cropType: 'Mushroom',
+      production: '500 kg'
+    },
+    {
+      id: 10,
+      name: 'Dumisani',
+      email: 'dumisani@gmail.com',
+      address: 'Thembisa',
+      farmSize: '84 hectare',
+      cropType: 'Mushroom',
+      production: '800 kg'
+    },
+    {
+      id: 11,
+      name: 'Teddy',
+      email: 'teddy@gmail.com',
+      address: 'Thembisa',
+      farmSize: '12 hectare',
+      cropType: 'Tomato',
+      production: '15 kg'
+    },
+    {
+      id: 12,
+      name: 'Bob',
+      email: 'bob@gmail.com',
+      address: 'Thembisa',
+      farmSize: '20 hectare',
+      cropType: 'Cherry Tomato',
+      production: '50 kg'
+    },
   ];
 
 export default farmersData;
