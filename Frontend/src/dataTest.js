f39a7bd01e4753077a038b1c2016bb772ae87733
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-24 11:30:41 +0200

    mock data

---
+++ Frontend/src/dataTest.js
@@ -0,0 +1,40 @@
+const farmersData = [
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@gmail.com',
+      address: '123 Main St',
+      farmSize: '50 hectare',
+      cropType: 'Wheat',
+      production: '1000 kg'
+    },
+    {
+      id: 2,
+      name: 'Neo',
+      email: 'neo@gmail.com',
+      address: 'Thembisa',
+      farmSize: '30 hectare',
+      cropType: 'Corn',
+      production: '800 kg'
+    },
+    {
+      id: 3,
+      name: 'Sicelumusa',
+      email: 'sicelumusa@gmail.com',
+      address: 'Thembisa',
+      farmSize: '85 hectare',
+      cropType: 'Tomato',
+      production: '1200 kg'
+    },
+    {
+      id: 4,
+      name: 'Simamnkele',
+      email: 'simamnkele@gmail.com',
+      address: 'Thembisa',
+      farmSize: '92 hectare',
+      cropType: 'Corn',
+      production: '900 kg'
+    },
+  ];
+
+export default farmersData;
