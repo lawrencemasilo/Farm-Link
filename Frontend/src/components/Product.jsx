82e3887ff0eb436de0cae0925fab7f3656a3e4e8
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-27 12:15:53 +0200

    added a basic template for the products

---
+++ Frontend/src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div className="product-container">
+      <div className="product-title-container">
+        <h1>Product</h1>
+      </div>
+    </div>
+  )
+}
