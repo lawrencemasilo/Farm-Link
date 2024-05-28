6cac9c473d68fd70601ff924de62af64a62b3f59
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-28 14:55:32 +0200

    Implemented the farmer home page

---
+++ Frontend/src/pages/HomeFarmer.jsx
@@ -0,0 +1,30 @@
+import '../styles/Home.css'
+import HeaderSignIn from '../components/HeaderSignIn'
+import SideBar from '../components/SideBar'
+import Members from '../components/Members'
+import { useState } from 'react'
+import Orders from '../components/Orders'
+import PlaceDelivery from '../components/PlaceDelivery'
+import Profile from '../components/Profile'
+import SideBarFarmer from '../components/SideBarFarmer'
+import Produce from '../components/Produce'
+
+export default function HomeFarmer() {
+  const [navItem, setNavItem] = useState('profile');
+  const [selected, setSelected] = useState('recent');
+  return (
+    <div className="home-container">
+      <div className="home-header-container">
+        {<HeaderSignIn />}
+      </div>
+      <div className="main-content-container">
+        {<SideBarFarmer setNavItem={setNavItem} setSelectedItem={setSelected} />}
+        {navItem === 'profile' && <Profile />}
+        {navItem === 'produce' && <Produce />}
+        {/*navItem === 'order' &&  <Orders />*/}
+        {/*navItem === 'members' &&  <Members />*/}
+        {/*navItem === 'place delivery' && <PlaceDelivery />*/}
+      </div>
+    </div>
+  )
+}
