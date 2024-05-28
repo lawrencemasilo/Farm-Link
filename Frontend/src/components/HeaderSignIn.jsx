650dfec9b4db42463a16dfe58b74c614a9110a04
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-28 22:01:41 +0200

    Made pages and components responsive

--- Frontend/src/components/HeaderSignIn.jsx
+++ Frontend/src/components/HeaderSignIn.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from 'react-router-dom'
 import '../styles/HeaderSignIn.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faGear, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faGear, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
 
 
 /*
@@ -12,6 +13,20 @@ import { faMagnifyingGlass, faGear, faUser } from '@fortawesome/free-solid-svg-i
 */
 export default function HeaderSignIn() {
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 720);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 720);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
 
   return (
     <div className="header_signin-container">
@@ -21,7 +36,7 @@ export default function HeaderSignIn() {
       <div className="header-nav">
         
       </div>
-      <div className="signin-wrapper">
+      {isMobile ? <FontAwesomeIcon icon={faBars} className="headerBar" onClick={() => setToggle((prev) => !prev)} /> : <div className="signin-wrapper">
         <div className="logout-btn-container">
           <div className="setting-container">
             <FontAwesomeIcon icon={faGear} className="settingsIcon"/>
@@ -35,7 +50,21 @@ export default function HeaderSignIn() {
             <button onClick={() => {navigate('/login')}}>Logout</button>
           </div>
         </div>
-      </div>
+      </div>}
+      {toggle && (
+        <div className="logout-btn-container-nav">
+          <div className="setting-nav">
+            <FontAwesomeIcon icon={faGear} className="settingsIconNav"/>
+            <p className="settings-title-nav">Settings</p>
+          </div>
+          <div className="profile-container-nav">
+            <FontAwesomeIcon icon={faUser} className="profileIconNav" />
+            <p className="userName-title-nav">User</p>
+          </div>
+          <div className="logout-container-nav">
+            <button onClick={() => {navigate('/login')}}>Logout</button>
+          </div>
+      </div>)}
     </div>
   )
 }
