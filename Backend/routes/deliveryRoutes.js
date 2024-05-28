102f266aae471b40298a1c3a600e699e88b6fbe0
Sicelumusa <musaqwabe@gmail.com>
2024-05-28 02:14:39 +0200

    Updated athethication file

--- Backend/routes/deliveryRoutes.js
+++ Backend/routes/deliveryRoutes.js
@@ -1,15 +1,15 @@
 // Maps URL endpoints to controller functions.
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require('../middleware/authMiddleware');
+const {cookieJwtAuth, authorizedRoles } = require('../middleware/crackCookie');
 
 // Importing delivery controller methods
 const { createDelivery, getDelivery, getDeliveries, updateDelivery, deleteDelivery } = require('../controllers/deliveryController');
 
-router.route('/delivery').post(isAuthenticated, createDelivery);
-router.route('/deliveries').get(isAuthenticated, getDeliveries);
-router.route('/delivery/:id').get(isAuthenticated, getDelivery);
-router.route('/delivery/:id').put(isAuthenticated, updateDelivery);
-router.route('/delivery/:id').delete(isAuthenticated, deleteDelivery);
+router.route('/delivery').post(cookieJwtAuth, createDelivery);
+router.route('/deliveries').get(cookieJwtAuth, getDeliveries);
+router.route('/delivery/:id').get(cookieJwtAuth, getDelivery);
+router.route('/delivery/:id').put(cookieJwtAuth, updateDelivery);
+router.route('/delivery/:id').delete(cookieJwtAuth, deleteDelivery);
 
 module.exports = router;
