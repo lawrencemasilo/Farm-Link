5a60447e081d6740f90c95e53e7208879598e4a9
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-29 18:39:18 +0200

    added a services component to the landing page

--- Frontend/src/components/Services.jsx
+++ Frontend/src/components/Services.jsx
@@ -1,28 +1,31 @@
 import '../styles/Services.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileSignature, faChartSimple, faBoxesPacking } from '@fortawesome/free-solid-svg-icons'
 
 export default function Services() {
   return (
-    <div className="services-container">
+    <div className="services-container" id="services">
         <div className="description-container">
             <h1 className="services-title">Our Service</h1>
         </div>
         <div className="services">
             <div className="service">
                 <div className="icon-container">
+                    <FontAwesomeIcon icon={faFileSignature} />
                 </div>
-                <h1 className="service-title">Manage Applications</h1>
+                <h1 className="service-title">Manage Orders</h1>
             </div>
             <div className="service">
-                <div className="icon-container"></div>
+                <div className="icon-container">
+                    <FontAwesomeIcon icon={faChartSimple} />
+                </div>
                 <h1 className="service-title">Track Produce Intake</h1>
             </div>
             <div className="service">
-                <div className="icon-container"></div>
-                <h1 className="service-title">Route Managemet</h1>
-            </div>
-            <div className="service">
-                <div className="icon-container"></div>
-                <h1 className="service-title">Placeholder</h1>
+                <div className="icon-container">
+                    <FontAwesomeIcon icon={faBoxesPacking} />
+                </div>
+                <h1 className="service-title">supplier Management</h1>
             </div>
         </div>
     </div>
