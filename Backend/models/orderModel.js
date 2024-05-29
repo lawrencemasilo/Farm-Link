1ac6acdca9923df9d8fbfd447c90779d64314416
Simamnkele <smambo85@gmail.com>
2024-05-29 18:06:45 +0200

    Linked farmer :id to order POST route

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -7,6 +7,11 @@ const orderSchema = new mongoose.Schema({
         required: true,
         ref: 'User'
     },
+    farmer: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Farm'
+    },
     name: {
         type: String,
         required: true
