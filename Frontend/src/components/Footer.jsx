ceb23940a7b806f86677cc15bf539856e8e4e4f8
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-29 18:38:13 +0200

    Added a footer to the landing page

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+        <div className="footer-wrapper">
+            <div className="footer-title-container">
+                <h1>Farm Link</h1>
+                <p>Link the future of farming with us.</p>
+                <div className="socials">
+                </div>
+            </div>
+            <div className="footer-page-containers">
+                <div className="footer-page-title">
+                    <h1>Pages</h1>
+                </div>
+                <p>Login</p>
+                <p>Register</p>
+            </div>
+        </div>
+    </div>
+  )
+}
