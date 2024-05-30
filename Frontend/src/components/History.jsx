202c532163aa645d1fcd0701dd6d2d1459aac0e9
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-30 10:49:37 +0200

    fetched current user id and fetched order data for said user

--- Frontend/src/components/History.jsx
+++ Frontend/src/components/History.jsx
@@ -1,58 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import '../styles/History.css'
+import { getOrder } from '../services/OrderService';
+import { profile } from '../services/ProfileService';
 
 export default function History() {
   const [select, setSelect] = useState('');
-  const [users, setUsers] = useState([]); 
-  const [farms, setFarms] = useState([]);
-  const [sortBy, setSortBy] = useState(false);
+  const [user, setUser] = useState();
+  const [id, setId] = useState();
+  const [orders, setOders] = useState();
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await profile();
+        setUser(userData.data);
+        setId(user._id)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userOrders = await getOrder(id);
+        setOrders(userOrders.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [])
+
   
   return (
     <div className="history-container">
       <div className="header-history-containers">
-        <div className="history-sort-container">
-          <button className="history-sort-by-container"></button>
-          {sortBy && <div className="history-sort-extent">
-            <div className="history-sort-options">
-              <p>Default</p>
-              <p>Name(asc)</p>
-              <p>Crop (asc)</p>
-              <p>Availability</p>
-            </div>
-          </div>}
-        </div>
-        <input type="text" className="searchHistoryContainer" placeholder="Search..." />
+
       </div>
       <div className="history-table-container">
         <table>
           <thead>
             <tr>
-              <th>Name</th>
+              <th>Crop Name</th>
               <th>Farm Size</th>
-              <th>Crop Type</th>
-              <th>Availability</th>
+              <th>Quantity</th>
             </tr>
           </thead>
           <tbody>
-            {/*users && users.map((user) => (
-              <tr key={user._id} onClick={() => handleClick(user._id)}>
-                <td>{user.name}</td>
-                <td>{user.farm ? `${user.farm.farmSize} ha` : 'No Farm'}</td>
-                <td>
-                  <ul>
-                    {user.farm && user.farm.crops ? user.farm.crops.map(crop => (
-                      <li key={crop._id}>{crop.cropName}</li>
-                    )) : <li>No Crops</li>}
-                  </ul>
-                </td>
-                <td>
-                  <ul>
-                    {user.farm && user.farm.crops ? user.farm.crops.map(crop => (
-                      <li key={crop._id}>{crop.availability} kg</li>
-                    )) : <li>-</li>}
-                  </ul>
-                </td>
-            </tr>
-          ))*/}
+            {orders && orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order.cropDetails.cropName}</td>
+                <td>{order.cropDetails.plotSize}</td>
+                <td>{order.quantity}</td>
+            </tr>))}
           </tbody>
         </table>
       </div>
