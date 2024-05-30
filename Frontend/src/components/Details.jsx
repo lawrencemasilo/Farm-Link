5d4d7f349ceb7db406aef346236d4e641f4ce9ef
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-30 18:46:19 +0200

    implemented a api put request to update the information

--- Frontend/src/components/Details.jsx
+++ Frontend/src/components/Details.jsx
@@ -3,7 +3,7 @@ import { profile } from '../services/ProfileService';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Details.css'
-import { farmerDatails, allFarmerDatails } from '../services/farmerService';
+import { farmerDatails, allFarmerDatails, updateFarmerDatails } from '../services/farmerService';
 
 //user
 /*
@@ -24,6 +24,7 @@ import { farmerDatails, allFarmerDatails } from '../services/farmerService';
 //name, location, streetName, houseNumber, city, farmSize
 export default function Details() {
   const [user, setUser] = useState();
+  const [farm, setFarm] = useState();
   const [id, setId] = useState();
   const [name, setName] = useState();
   const [email, setEmail] = useState();
@@ -33,6 +34,7 @@ export default function Details() {
   const [houseNumber, setHouseNumber] = useState();
   const [city, setCity] = useState();
   const [farmSize, setFarmSize] = useState();
+  const [selected, setSelected] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,7 +43,9 @@ export default function Details() {
         setUser(data.data);
         if (data.data._id) {
           setId(data.data._id)
-          //console.log(data.data._id)
+          setName(data.data.name)
+          setEmail(data.data.email)
+          setPhone(data.data.phone)
         }
       } catch(err) {
         console.log(err);
@@ -54,14 +58,30 @@ export default function Details() {
     const fetchData = async () => {
       try {
         const data = await allFarmerDatails();
-        console.log(data);
-
+        setFarm(data.data);
+        setLocation(data.data.location)
+        setStreetName(data.data.streetName)
+        setHouseNumber(data.data.houseNumber)
+        setCity(data.data.city)
+        setFarmSize(data.data.farmSize)
       } catch(err) {
         console.log(err);
       }
     } 
     fetchData();
   }, [])
+
+  //console.log({location, streetName, houseNumber, city, farmSize})
+  const handleUpdate = async () => {
+    if (selected) {
+      try {
+        const data = await updateFarmerDatails({name, location, streetName, houseNumber, city, farmSize});
+        console.log("update sent")
+      } catch(err) {
+        console.log(err);
+      }
+    }
+  }
 
   return (
     <div className="details-container">
@@ -86,43 +106,58 @@ export default function Details() {
           <div className="details-phone-container">
             <p className="details-phone-title details-title">Phone</p>
             <div className="details-phone-btn">
-              <input type="number" placeholder={user && user.phone} className="details-input-container" />
+              <p className="details-phone" >{user && user.phone}</p>
             </div>
           </div>
-          <div className="details-location-container">
+          <div className="details-location-container" onClick={() => setSelected('location')}>
             <p className="details-location-title details-title">Location</p>
-            <div className="details-location-btn">
-              <input type="number" placeholder="" className="details-input-container" required />
-            </div>
+            {!farm || selected == 'location'? <div className="details-location-btn">
+              <input type="text" placeholder="" className="details-input-container" required onChange={(e) => setLocation(e.target.value)} />
+            </div>:
+            <div className="details-location2-container">
+              <p className="details-location details-value">{farm && farm.location}</p>
+            </div>}
           </div>
-          <div className="details-street-container">
+          <div className="details-street-container" onClick={() => setSelected('street')}>
             <p className="details-street-title details-title">Street</p>
-            <div className="details-street-btn">
-              <input type="number" placeholder="" className="details-input-container" required />
-            </div>
+            {!farm || selected == 'street' ? <div className="details-street-btn">
+              <input type="text" placeholder="" className="details-input-container" required onChange={(e) => setStreetName(e.target.value)}/>
+            </div>: 
+            <div className="details-street2-container">
+              <p className="details-street details-value">{farm && farm.streetName}</p>
+            </div>}
           </div>
-          <div className="details-house-container">
-            <p className="details-house-title details-title">House Name</p>
-            <div className="details-house-btn">
-              <input type="number" placeholder="" className="details-input-container" required />
-            </div>
+          <div className="details-house-container" onClick={() => setSelected('house')}>
+            <p className="details-house-title details-title">House Number</p>
+            {!farm || selected == 'house' ?<div className="details-house-btn">
+              <input type="number" placeholder="" className="details-input-container" required onChange={(e) => setHouseNumber(e.target.value)} />
+            </div>:
+            <div className="details-house2-container">
+              <p className="details-house details-value">{farm && farm.houseNumber}</p>
+            </div>}
           </div>
-          <div className="details-city-container">
+          <div className="details-city-container" onClick={() => setSelected('city')}>
             <p className="details-city-title details-title">City</p>
-            <div className="details-city-btn">
-              <input type="number" placeholder="" className="details-input-container" required />
-            </div>
+            {!farm || selected == 'city' ?<div className="details-city-btn">
+              <input type="text" placeholder="" className="details-input-container" required onChange={(e) => setCity(e.target.value)}/>
+            </div>:
+            <div className="details-city2-container">
+              <p className="details-city details-value">{farm && farm.city}</p>
+            </div>}
           </div>
-          <div className=" details-plot-container">
+          <div className=" details-plot-container" onClick={() => setSelected('plot')}>
             <p className="details-title">Plot Size</p>
-            <div className="details-plot-btn">
-              <input type="number" placeholder="" className="details-input-container" required />
-            </div>
+            {!farm || selected == 'plot' ?<div className="details-plot-btn">
+              <input type="number" placeholder="" className="details-input-container" required onChange={(e) => setFarmSize(e.target.value)}/>
+            </div>:
+            <div className="details-plot2-container">
+              <p className="details-plot details-value">{farm && farm.farmSize}</p>
+            </div>}
           </div>
         </div>
-        <div className="add-produce-container">
-          <FontAwesomeIcon icon={faCirclePlus} className="add-produceIcon" />
-          <p>Add</p>
+        <div className="update-details-container" onClick={handleUpdate}>
+          <FontAwesomeIcon icon={faCirclePlus} className="update-detailsIcon" />
+          <p>Update</p>
         </div>
       </div>
     </div>
