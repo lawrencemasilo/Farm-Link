eb4db48e188aefd94f1f22865086a0e61a5910cb
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-06-08 19:10:59 +0200

    rendered the Map component

--- Frontend/src/components/PlaceDelivery.jsx
+++ Frontend/src/components/PlaceDelivery.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/PlaceDelivery.css'
+import Schedule from './Schedule'
+import MapBox from './MapBox'
+import Map from './Map';
 
 export default function PlaceDelivery() {
+
   return (
-    <div className="place-delivery-container">PlaceDelivery</div>
+    <div className="place-delivery-container">
+      {<Map />}
+    </div>
   )
 }
