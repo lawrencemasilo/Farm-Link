f08daba7517b0da85e648fbba5a7b2bf499277e5
Neo Masilo <neolawrencemasilo@gmail.com>
2024-06-11 17:10:11 +0200

    Fetched user data

--- Frontend/src/pages/HomeFarmer.jsx
+++ Frontend/src/pages/HomeFarmer.jsx
@@ -1,6 +1,6 @@
 import '../styles/Home.css'
 import HeaderSignIn from '../components/HeaderSignIn'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Profile from '../components/Profile'
 import SideBarFarmer from '../components/SideBarFarmer'
 import Produce from '../components/Produce'
@@ -8,13 +8,56 @@ import Settings from '../components/Settings'
 import { ThemeContext } from '../contexts/ThemeContext'
 import { UserContext } from '../contexts/UserContext'
 import { FarmPopUp } from '../components/FarmPopUp'
+import { allFarmerDatails, updateFarmerDatails } from '../services/farmerService'
 
 export default function HomeFarmer() {
   const [navItem, setNavItem] = useState('profile');
   const { theme } = useContext(ThemeContext);
   const [popUp, setPopUp] = useState(true);
   const { user } = useContext(UserContext);
-  
+  const [coordinates, setCoordinates] = useState({});
+  const [farm, setFarm] = useState();
+  const [id, setId] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [phone, setPhone] = useState();
+  const [location, setLocation] = useState();
+  const [streetName, setStreetName] = useState();
+  const [houseNumber, setHouseNumber] = useState();
+  const [city, setCity] = useState();
+  const [farmSize, setFarmSize] = useState();
+
+  useEffect(() => {
+    //fetches the information of the current user
+      try {
+        if (user._id) {
+          setId(user._id)
+          setName(user.name)
+          setEmail(user.email)
+          setPhone(user.phone)
+        }
+      } catch(err) {
+        console.log(err);
+      }
+  }, [])
+
+  useEffect(() => {
+    //fetches all the information of the farmer
+    const fetchData = async () => {
+      try {
+        const data = await allFarmerDatails();
+        setFarm(data.data);
+        setLocation(data.data.location)
+        setStreetName(data.data.streetName)
+        setHouseNumber(data.data.houseNumber)
+        setCity(data.data.city)
+        setFarmSize(data.data.farmSize)
+      } catch(err) {
+        console.log(err);
+      }
+    } 
+    fetchData();
+  }, [])
 
   const handleGetLocation = () => {
     navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
@@ -24,11 +67,21 @@ export default function HomeFarmer() {
     
 
   const successLocation = (position) => {
-    console.log(position) //test
+    setCoordinates( position.coords.latitude, position.coords.longitude) //test
   }
 
   const errorLocation = (error) => {
     console.log(error) //test
+  }
+
+  const handleUpdate = async () => {
+    //updates the current users information
+      try {
+        const data = await updateFarmerDatails({name, location, streetName, houseNumber, city, farmSize, coordinates});
+        console.log("update sent")
+      } catch(err) {
+        console.log(err);
+      }
   }
 
   return (
@@ -38,9 +91,10 @@ export default function HomeFarmer() {
       </div>
       <div className="main-content-container">
         {/*getting current Location test */}
-        {user && handleGetLocation()}
+        {handleGetLocation()}
         {/*getting current Location test */}
-        {user && user.farm  && popUp && <FarmPopUp setPopUp={setPopUp} />}
+        {coordinates && handleUpdate}
+        {/*user && user.farm  && popUp && <FarmPopUp setPopUp={setPopUp} coordinates={coordinates} />*/}
         {<SideBarFarmer setNavItem={setNavItem} />}
         {navItem === 'profile' && <Profile />}
         {navItem === 'produce' && <Produce />}
